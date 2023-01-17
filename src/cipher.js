83d6bcfd4316dd455792105d482c5daf2f239497
ajuliamm <ajuliammelo@gmail.com>
2023-01-17 18:49:38 -0300

    Funcionalidade de botões

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,13 @@
-const cipher = {
+/*const cipher = {
+
   // ...
 };
 
 export default cipher;
+
+cipher.encode(offset, string);
+// offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
+
+cipher.decode(offset, string);
+ //offset é o número de posições que queremos mover para a esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
+*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import cipher from './cipher.js';
+/*import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher);*/
+
+//src/index.js: aqui você deve escutar os eventos de DOM, chamar cipher.encode() e cipher.decode().
+
+//BOTÃO PARA CRIPTOGRAFAR
+const encrypt = document.getElementById('encrypt')
+encrypt.addEventListener('click', encryptText)
+
+function encryptText(){
+alert('ok')
+}
+
+
+//BOTÃO PARA DESCRIPTOGRAFAR
+const decrypt = document.getElementById('decrypt')
+decrypt.addEventListener('click', decryptText)
+
+function decryptText(){
+alert('ok!')
+}
